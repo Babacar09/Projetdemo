5b54c830ba05ac91c2dd5c9f7b3a4ecdbe11864a
babacar dieye <dieyebous92@gmail.com>
2024-12-29 15:54:29 +0100

    commi14

---
+++ Tontine/CotisationManu/CotisationAccueilManu.js
@@ -0,0 +1,31 @@
+let solde = parseFloat(localStorage.getItem("solde")) || 0;
+document.getElementById("currentSolde").textContent = `Solde actuel:${solde}$`;
+
+document.getElementById("debitForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const amount = parseFloat(document.getElementById("amount").value);
+  const monAffich = document.getElementById('AfficheAmount')
+  console.log(monAffich);
+  
+  if (amount > solde) {
+    document.getElementById("result").textContent = "Erreur solde insuffisant";
+    document.getElementById("result").style.color = "red";
+  } else {
+    solde -= amount;
+    localStorage.setItem("solde", solde);
+
+    document.getElementById(
+      "result"
+    ).textContent = `Débit réussi. Nouveau solde : ${solde}€`;
+    document.getElementById("result").style.color = "green";
+
+    document.getElementById(
+      "currentSolde"
+    ).textContent = `Solde actuel : ${solde}€`;
+
+  }
+});
+
+document.getElementById("backToBalance").addEventListener("click", function () {
+  window.location.href = "http://127.0.0.1:5500/index.html";
+});
